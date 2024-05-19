64b819b3450087e3241a140118dafd7495c88483
Omar Haikal <popmandadada@gmail.com>
2024-05-20 01:00:20 +0400

    Modified LSP, added i3 to config

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -8,7 +8,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = {"lua_ls"}
+        ensure_installed = {"lua_ls", "jedi_language_server"}
       })
     end
   },
@@ -16,8 +16,13 @@ return {
     "neovim/nvim-lspconfig",
     config = function()
       local lspconfig = require("lspconfig")
+      -- Setup for Lua LSP with default settings
       lspconfig.lua_ls.setup({})
-      vim.keymap.set('n', '<leader>i', vim.lsp.buf.hover, { desc = 'LSP test' })
+      -- Setup for Jedi Language Server with default settings
+      lspconfig.jedi_language_server.setup({})
+      vim.keymap.set('n', '<leader>i', vim.lsp.buf.hover, {})
+      vim.keymap.set('n', '<leader>d', vim.lsp.buf.definition, {})
+      vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
     end
   }
 }
